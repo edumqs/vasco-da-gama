3e029b089d140ac9845d2cb101c503ecd5ece9e2
logeybogey <michaeldcjohnson@gmail.com>
2021-01-21 21:57:13 +0600

    changed destination when user signs out

--- clientSrc/components/Header.js
+++ clientSrc/components/Header.js
@@ -8,7 +8,7 @@ export default function Header(props) {
             <a className="navbar-brand" href="registration">Register</a></div>;
     } else {
         navbarRight = <div><a href='/profile' className="navbar-brand">Welcome {props.user}</a>
-            <a className="navbar-brand" href="landing" onClick={props.updateSignedIn}>Sign Out</a></div>;
+            <a className="navbar-brand" href="/" onClick={props.updateSignedIn}>Sign Out</a></div>;
     }
     return (
         <nav className="navbar navbar-dark bg-primary">
