9eb63dbe60c96a8916afe6976bf196d1fda11100
logeybogey <michaeldcjohnson@gmail.com>
2021-01-25 10:12:46 +0600

    updated webpack and installed 'file-loader' to include svg file on landing page

--- clientSrc/components/Landing.js
+++ clientSrc/components/Landing.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import travelImage from '../travel.svg';
 import '../styles/Landing.less';
 
 export default function Landing(props) {
     if (props.signedIn === false) {
         return (
             <div className='landing-container'>
+                <img className='landing-svg' src={travelImage}></img>
                 <div className='landing-blurb'>
                     <h2>“Vasco da Gama” - Explore the world and meet others in your vicinity!</h2>
                 </div>
@@ -14,6 +16,7 @@ export default function Landing(props) {
     }
     return (
         <div className='landing-container'>
+            <img className='landing-svg' src={travelImage}></img>
             <div className='landing-blurb'>
                 <h2>“Vasco da Gama” - Explore the world and meet others in your vicinity!</h2>
             </div>
